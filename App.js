ce43c935b459952d3b7473aef7212aaf3eb1c0c3
Jinanshshah17 <202312065@daiict.ac.in>
2025-04-24 16:11:27 +0530

    Intital Commit

---
+++ App.js
@@ -0,0 +1,38 @@
+import { Text, SafeAreaView, StyleSheet,View } from 'react-native';
+import React from 'react';
+import {WebView} from 'react-native-webview';
+
+
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
+
+// or any files within the Snack
+import AssetExample from './components/AssetExample';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+  
+      <WebView 
+      source={{uri:'metamerchandice.com'}}
+      style={styles.WebView}
+      startInLoadingState={true}
+       />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
